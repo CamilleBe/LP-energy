2e9713590194f4986385875ed59398a6263a5172
Camille <97457952+CamilleBe@users.noreply.github.com>
2023-04-17 10:45:47 +0200

    fav

--- home-page/lpPompeChaleur/pompechaleur.php
+++ home-page/lpPompeChaleur/pompechaleur.php
@@ -95,6 +95,7 @@ $email = $_POST['email'] ?? "";
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="stylesheet" href="css/pompechaleur.css">
+     <link rel="icon" type="image/x-icon" href="img/favicon.svg">
 </head>
 
 <body>
